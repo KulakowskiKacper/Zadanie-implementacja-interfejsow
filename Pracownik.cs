49e089e4815d66bfd6903b1ee521bcd8216027b2
Kacper Kułakowski <kacper.kulakowski@microsoft.wsei.edu.pl>
2024-03-11 10:21:13 +0100

    Dodano klase pracownik

---
+++ Pracownik.cs
@@ -0,0 +1,7 @@
+namespace ImplementacjaInterfejsow
+{
+    public class Pracownik
+    {
+        
+    }
+}
--- obj/Debug/net8.0/ImplementacjaInterfejsow.AssemblyInfo.cs
+++ obj/Debug/net8.0/ImplementacjaInterfejsow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImplementacjaInterfejsow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d3df49fd45ede41aa10b6921edd7ae263819b78")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImplementacjaInterfejsow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImplementacjaInterfejsow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
