6d89c2eb51041c0fbb2e832b2838c3f1a519e022
Kacper Kułakowski <kacper.kulakowski@microsoft.wsei.edu.pl>
2024-03-11 10:26:16 +0100

    Dodano properties dla pól

--- Pracownik.cs
+++ Pracownik.cs
@@ -1,7 +1,45 @@
+using System.ComponentModel;
+using System.Data;
+
 namespace ImplementacjaInterfejsow
 {
     public class Pracownik
     {
-        
+        private string nazwisko;
+        public string Nazwisko
+        {
+            get { return nazwisko; }
+            set 
+            { nazwisko = value.Trim(); }
+        }
+        private DateTime dataZatrudnienia;
+        public DateTime DataZatrudnienia
+        {
+            get { return dataZatrudnienia; }
+            set 
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new DataException("Data zatrudnienia nie może być z przyszłości");
+                }
+                 dataZatrudnienia = value;
+            }
+        }
+        private double wynagrodzenie;
+        public double Wynagrodzenie
+        {
+            get { return wynagrodzenie; }
+            set 
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                    
+                }
+                else{
+                    wynagrodzenie = value;
+                    }
+            }
+        }
     }
 }
